d6ebb84936a30ca8ce48a39663b1f99966659e87
stevenzheng33 <stevenzheng33@163.com>
2022-05-15 21:32:35 -0700

    update

--- source.py
+++ source.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
-
+st.title('Heart Disease Analysis')
 df = pd.read_csv('death_causes.csv')
-df.set_index('Year')
-st.write("Here's our first attempt at using data to create a table:")
+df = df.set_index('Year')
+st.write("This is a dataframe displaying cause of deaths")
 st.write(df)
-st.bar_chart(df[['Heart disease']])
+st.bar_chart(df['Heart disease'])
